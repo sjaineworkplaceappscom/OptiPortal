4015fa7a49d9c6508db0fd0e5c88fed1f09c08b8
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-05-23 19:33:53 +0530

    set base structure for theme css file and new setup of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(){
+    // Apply default theme css
+    var cssfile = '<link rel="stylesheet" href="../assets/css/theme/kendo.custom.blue.opal.css?v=' + (new Date()).getTime() + '" />';
+    document.write(cssfile);
+  }
+  
+  ngOnInit(){
+    this.manageThemeCssFile();  
+  }
+
+  // Change theme css file
+  manageThemeCssFile(){
+    $(document).ready(function(){
+        $('#flat').click(function (){
+            $('footer').find('link.opti__theme-css-file').remove();  
+            $('footer').append('<link rel="stylesheet" href="../assets/css/theme/kendo.custom.flat.css?v=' + (new Date()).getTime() + '" class="opti__theme-css-file" type="text/css" />');
+        });
+        $('#opal').click(function (){
+            $('footer').find('link.opti__theme-css-file').remove();
+            $('footer').append('<link rel="stylesheet" href="../assets/css/theme/kendo.custom.blue.opal.css?v= ' + (new Date()).getTime() + '" class="opti__theme-css-file" type="text/css" />');
+        });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
