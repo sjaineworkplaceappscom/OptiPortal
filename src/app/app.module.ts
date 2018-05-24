2c99cb3abca459dd56d3d4b1b7e3849578491207
sjaineworkplaceappscom <sjain@eworkplaceapps.com>
2018-05-24 12:26:10 +0530

    Added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { LeftComponent } from './common/left/left.component';
+import { RightComponent } from './common/right/right.component';
+import { TopComponent } from './common/top/top.component';
+import { MainContentComponent } from './common/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent,
+    TopComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
