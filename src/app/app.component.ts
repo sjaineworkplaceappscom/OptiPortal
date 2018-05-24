94815911817d4486d9e186c99aad1cea0a54d4af
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-05-24 14:40:52 +0530

    structure changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,19 +16,29 @@ export class AppComponent {
   
   ngOnInit(){
     this.manageThemeCssFile();  
+    this.manageSidebarAndRightSectionWidth();
   }
 
-  // Change theme css file
+  // start apply theme css
   manageThemeCssFile(){
     $(document).ready(function(){
         $('#flat').click(function (){
             $('footer').find('link.opti__theme-css-file').remove();  
             $('footer').append('<link rel="stylesheet" href="../assets/css/theme/kendo.custom.flat.css?v=' + (new Date()).getTime() + '" class="opti__theme-css-file" type="text/css" />');
         });
+
         $('#opal').click(function (){
             $('footer').find('link.opti__theme-css-file').remove();
             $('footer').append('<link rel="stylesheet" href="../assets/css/theme/kendo.custom.blue.opal.css?v= ' + (new Date()).getTime() + '" class="opti__theme-css-file" type="text/css" />');
         });
     });
+  }
+
+  // start sidebar and right panel manage
+  manageSidebarAndRightSectionWidth(){
+      document.getElementById('sidebarCollapse').onclick = function() {
+        document.getElementById('opti_LeftPanelID').classList.toggle('opti_sidebar-minimize');  
+        document.getElementById('opti_RightPanelID').classList.toggle('opti_sidebar-minimize');
+      };
   }
 }
