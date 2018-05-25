ddcece527fc798c6254987cada15cfe4aad234a8
sjaineworkplaceappscom <sjain@eworkplaceapps.com>
2018-05-25 14:54:57 +0530

    Changes

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router:Router){
+
+  }
+  canActivate(next: ActivatedRouteSnapshot,    state: RouterStateSnapshot): boolean {     
+    return this.canActivated();
+  }
+
+  private canActivated():boolean{
+    // get authenticated flag from db.
+    let authenticated:boolean=localStorage.getItem('AccessToken')!= null;    
+
+    // if not authenticated redirect to login page.
+    if(authenticated==false)
+    {
+     this.router.navigate(['/login']);
+    }    
+    return authenticated;
+  }
+}
