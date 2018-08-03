be60e0b3ffbf2e796322a095c16a56d452fe6ae2
Aryan Vyas <avyas@optiproerp.com>
2018-08-03 14:57:19 +0530

    Purchase Inquiry add service

--- src/app/services/purchase-enquiry.service.ts
+++ src/app/services/purchase-enquiry.service.ts
@@ -3,6 +3,7 @@ import { environment } from '../../environments/environment';
 import { HttpHelper } from '../helpers/http.helper';
 import { Observable } from '../../../node_modules/rxjs/Observable';
 import { RequestOptions } from '../../../node_modules/@angular/http';
+import { TempPurchaseInquiryModel } from '../tempmodels/temppurchase-inquiry';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,20 @@ export class PurchaseEnquiryService {
     let url: string = this.baseUrl + "purchaseinquiry/list";
   //let reqOption: RequestOptions = new RequestOptions({ method: RequestMethod.Get, headers: headers });
    return this.httpHelper.get(url,null);   
+  }
+
+  /**
+   * AddPurchaseInquiry
+   */
+  public AddPurchaseInquiry(request: TempPurchaseInquiryModel):Observable<any> {
+    var data: any = {
+      "customerId": request.Customer,
+      "customerName": request.Name,
+      "status": request.Status,
+      "buyer": request.BuyerCreatedBy,
+      "validUntil": request.ValidUntil,
+      "refrence": request.Reference
+    }
+    return this.httpHelper.post(this.baseUrl + "purchaseinquiry/add",data,null);
   }
 }
