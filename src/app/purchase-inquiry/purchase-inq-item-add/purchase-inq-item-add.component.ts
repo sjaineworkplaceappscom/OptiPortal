ccec3fc188529f7a386bce282f9088d2e115c6b3
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-08-13 19:33:59 +0530

    manage grid and form section for item tab

--- src/app/purchase-inquiry/purchase-inq-item-add/purchase-inq-item-add.component.ts
+++ src/app/purchase-inquiry/purchase-inq-item-add/purchase-inq-item-add.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { TempPurchaseInquiryItemModel } from '../../tempmodels/temppurchase-inquiry-item';
 import { TempPurchaseInquiryModel } from '../../tempmodels/temppurchase-inquiry';
+import { UIHelper } from '../../helpers/ui.helpers';
 
 @Component({
   selector: 'app-purchase-inq-item-add',
@@ -9,10 +10,70 @@ import { TempPurchaseInquiryModel } from '../../tempmodels/temppurchase-inquiry'
 })
 export class PurchaseInqItemAddComponent implements OnInit {
 
+  /**
+   * global variable
+  */
+  isMobile: boolean;
+  gridHeight: number;
+
+  /**
+   * Item tab Variable
+  */
+  addItem:boolean = false;
+  itemGrid:boolean = true;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    /**
+     * Apply Grid Height
+    */
+    this.gridHeight = UIHelper.getMainContentHeight();
+      
+    /**
+     * Check Mobile device
+    */
+      this.isMobile = UIHelper.isMobile();
+  }
+
+
   constructor() { }
 
   ngOnInit() {
-    
+    /**
+     * Apply Grid Height
+    */
+    this.gridHeight = UIHelper.getMainContentHeight();
+        
+    /**
+      * Check Mobile device
+    */
+      this.isMobile = UIHelper.isMobile();
+  }
+
+  showItemsGrid(){
+    this.addItem = false;
+    this.itemGrid = true;
+  }
+
+  showItemForm(){
+    this.addItem = true;
+    this.itemGrid = false;
+  }
+
+  showAddItemSection(){
+    this.showItemForm();
+  }
+
+  AddPurchaseInquiryItem(){
+    this.showItemsGrid();
+  }
+
+  addItemtagain(){
+    this.showItemsGrid();
+  }
+
+  closeItemForm(){
+    this.showItemsGrid();
   }
 
 
