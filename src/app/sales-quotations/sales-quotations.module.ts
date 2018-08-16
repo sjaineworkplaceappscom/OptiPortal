4d280644342d7e5c2d5d4b549cfdf03f28205d92
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-08-16 12:32:02 +0530

    small changes in structure and add grid for sales quotations

--- src/app/sales-quotations/sales-quotations.module.ts
+++ src/app/sales-quotations/sales-quotations.module.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { SalesQuotationsRoutingModule } from './sales-quotations-routing.module';
 import { SalesQuotationsComponent } from './sales-quotations.component';
 import { SalesQuotationsListComponent } from './sales-quotations-list/sales-quotations-list.component';
+import { GridModule, ExcelModule } from '@progress/kendo-angular-grid';
+import { FormsModule } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   imports: [
     CommonModule,
-    SalesQuotationsRoutingModule
+    SalesQuotationsRoutingModule,
+    GridModule,
+    ExcelModule,
+    FormsModule,
+    AngularSvgIconModule
   ],
   declarations: [SalesQuotationsComponent, SalesQuotationsListComponent],
   exports:[SalesQuotationsComponent]
