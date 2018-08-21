8b43bbda3cb849e42490a0b1516faf44107718cc
Shashank Jain <sjain@optiproerp.com>
2018-08-21 19:24:56 +0530

    config

--- src/assets/configuration.ts
+++ src/assets/configuration.ts
@@ -1,10 +1,10 @@
 export const Configuration={
     // For Dev
-    // baseServerAPIEndpoint:"http://172.16.6.139:9090/"
+     baseServerAPIEndpoint:"http://172.16.6.139:9090/"
     // For Demo
     // baseServerAPIEndpoint:"http://139.144.10.218:8080/"
     // Local
-    baseServerAPIEndpoint:"http://localhost:56986/"
+    //baseServerAPIEndpoint:"http://localhost:56986/"
 
     ,firstHomePage:"/home/purchaseinquiry"
     , dateFormat:'dd MMM yy'
