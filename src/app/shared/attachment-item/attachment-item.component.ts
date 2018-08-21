043b936460a5d646a72a8ddfbbf7c026b6b2c2ef
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-08-21 14:53:07 +0530

    attachment grid, add class on body for all module

--- src/app/shared/attachment-item/attachment-item.component.ts
+++ src/app/shared/attachment-item/attachment-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { UIHelper } from '../../helpers/ui.helpers';
 
 @Component({
   selector: 'app-attachment-item',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttachmentItemComponent implements OnInit {
 
+    /**
+     * global variable
+    */
+   isMobile: boolean;
+   gridHeight: number;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+      //Apply Grid Height
+      this.gridHeight = UIHelper.getMainContentHeight();
+      
+      // Check Mobile device
+      this.isMobile = UIHelper.isMobile();
+  }
+
   constructor() { }
 
   ngOnInit() {
+    //Apply Grid Height
+    this.gridHeight = UIHelper.getMainContentHeight();
+
+    // Check Mobile device
+    this.isMobile = UIHelper.isMobile();
   }
 
 }
