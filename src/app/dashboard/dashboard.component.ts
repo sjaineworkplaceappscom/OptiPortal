98a463add4a88c25d318aee2087aba5cb20b224d
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-08-27 12:37:08 +0530

    scrollbar issue for item ad

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { UIHelper } from '../helpers/ui.helpers';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,15 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  isMobile: boolean;
+  gridHeight: number;
+  showLoader: boolean = false;
+  searchRequest: string = '';
+  
+  // UI Section
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    // apply grid height
+    this.gridHeight = UIHelper.getMainContentHeight();
+
+    // check mobile device
+    this.isMobile = UIHelper.isMobile();
+  }
+  // End UI Section
+
   constructor() { }
 
   ngOnInit() {
+    // check mobile device
+    this.isMobile = UIHelper.isMobile();
+
     // Apply class on body start
     const element = document.getElementsByTagName("body")[0];
     element.className = "";
     element.classList.add("opti_body-dashboard");
     element.classList.add("opti_body-main-module");
     // Apply class on body end
+
+    this.renderChart();
+  }
+
+  /**
+   * RenderChart
+  */
+  public renderChart() {
+    this.showLoader = true;
+    setTimeout(()=>{    
+      this.showLoader = false;
+    }, 1000);
   }
 
 }
