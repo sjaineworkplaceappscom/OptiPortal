552a45e330372980f42cde631b8f2f5fba8ad4e3
Shashank Jain <sjain@optiproerp.com>
2018-08-31 17:47:56 +0530

    change

--- src/assets/configuration.ts
+++ src/assets/configuration.ts
@@ -2,9 +2,9 @@ import { hasFilterMenu } from "../../node_modules/@progress/kendo-angular-grid/d
 
 export const Configuration={
     // For Dev
-    baseServerAPIEndpoint:"http://172.16.6.139:9090/"
+    // baseServerAPIEndpoint:"http://172.16.6.139:9090/"
     // For Demo
-    // baseServerAPIEndpoint:"http://139.144.10.218:8080/"
+    baseServerAPIEndpoint:"http://139.144.10.218:8080/"
     // Local
     // baseServerAPIEndpoint:"http://localhost:56986/"
 
