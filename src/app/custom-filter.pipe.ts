ba6084c021ddd174419501f997eb9edeee3ea900
Shashank Jain <sjain@optiproerp.com>
2018-09-20 15:51:00 +0530

    Added search

--- src/app/custom-filter.pipe.ts
+++ src/app/custom-filter.pipe.ts
@@ -14,10 +14,36 @@ export class CustomFilterPipe implements PipeTransform {
       args=args.toLowerCase();
     }
    
-    return value.filter(items => {      
-      return (items.Notes!=null && items.Notes.toLowerCase().indexOf(args) != -1 ) || 
-      (items.NoteText!=null && items.NoteText.toLowerCase().indexOf(args) != -1 )
+    return value.filter(items => { 
       
+      // Notes
+      return (items.Notes!=null && items.Notes.toLowerCase().indexOf(args) != -1 ) ||       
+      (items.NoteText!=null && items.NoteText.toLowerCase().indexOf(args) != -1 )||
+
+      //Sales Order
+      (items.OrderNumber!=null && items.OrderNumber.toString().toLowerCase().indexOf(args) != -1 ) ||
+      (items.Remark!=null && items.Remark.toLowerCase().indexOf(args) != -1 ) ||
+      (items.SalesEmployee!=null && items.SalesEmployee.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Owner!=null && items.Owner.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Total!=null && items.Total.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Servicetype!=null && items.Servicetype.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Status!=null && items.Status.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Branch!=null && items.Branch.toLowerCase().indexOf(args) != -1 ) ||
+      (items.DeliveryDate!=null && items.DeliveryDate.toString().toLowerCase().indexOf(args) != -1 ) ||
+      (items.DocumentDate!=null && items.DocumentDate.toString().toLowerCase().indexOf(args) != -1 ) ||
+
+      //Sales Qutation
+      (items.QuotationNumber!=null && items.QuotationNumber.toString().toLowerCase().indexOf(args) != -1 ) ||
+      (items.Remark!=null && items.Remark.toLowerCase().indexOf(args) != -1 ) ||
+      (items.SalesEmployee!=null && items.SalesEmployee.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Owner!=null && items.Owner.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Total!=null && items.Total.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Servicetype!=null && items.Servicetype.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Status!=null && items.Status.toLowerCase().indexOf(args) != -1 ) ||
+      (items.Branch!=null && items.Branch.toLowerCase().indexOf(args) != -1 ) ||
+      (items.QuotationDate!=null && items.QuotationDate.toString().toLowerCase().indexOf(args) != -1 ) ||
+      (items.Duedate!=null && items.Duedate.toString().toLowerCase().indexOf(args) != -1 ) ||
+      (items.DocumentDate!=null && items.DocumentDate.toString().toLowerCase().indexOf(args) != -1 ) 
 
       
     });
