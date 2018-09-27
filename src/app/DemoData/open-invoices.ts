9971581ea7e19a33134fc501acec11d97cf68b51
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-09-27 19:11:20 +0530

    open invoices list

---
+++ src/app/DemoData/open-invoices.ts
@@ -0,0 +1,121 @@
+// List
+export const openInvoicesList = [  
+    {  
+        "Invoice":"Cust001",
+        "invoiceDate":"01/07/2018",
+        "dueDate":"01/07/2018",
+        "Amount":"1250",
+        "Freight":"",
+        "Tax":"10",
+        "totalAmount":"1240",
+        "billToAddress":"Indore",
+        "contactPerson":"Prashant"
+    }
+ ,  
+    {  
+        "Invoice":"Cust002",
+        "invoiceDate":"01/07/2018",
+        "dueDate":"01/07/2018",
+        "Amount":"1260",
+        "Freight":"",
+        "Tax":"10",
+        "totalAmount":"1250",
+        "billToAddress":"Bhopal",
+        "contactPerson":"Ankur"
+    }
+]
+
+
+// Content Tab
+export const deliveryNotesContent = [  
+    {  
+        "Line":"item01",
+        "itemCode":"20",
+        "Quantity":"5210",
+        "UOM":"5",
+        "Price":"5",
+        "Tax":"code1",
+        "totalPrice":"5200",
+        "salesOrder":'UOM12',
+        "salesOrderLine":"Indore",
+        "shipToLocation":"Indore"
+    }
+ ,  
+    {  
+        "Line":"item01",
+        "itemCode":"20",
+        "Quantity":"5210",
+        "UOM":"5",
+        "Price":"5",
+        "Tax":"code1",
+        "totalPrice":"5200",
+        "salesOrder":'UOM12',
+        "salesOrderLine":"Indore",
+        "shipToLocation":"Indore"
+    }
+]
+
+// Attchement tab
+export const deliveryNotesAttachment = [  
+    {  
+        "AttachmentName":"Report1.png",
+        "SizeInKb":"10KB",
+        "AttachmentTypeText":"PNG",
+        "CreatedByName":"Prashant"
+    }
+ ,  
+    {  
+        "AttachmentName":"Report2.png",
+        "SizeInKb":"10KB",
+        "AttachmentTypeText":"PNG",
+        "CreatedByName":"Prashant"
+    }
+]
+
+// Notes
+export const deliveryNotesTabNotes = [  
+    {  
+        "CreatedByName":"prashant",
+        "CreatedDate":"05/07/2018",
+        "Notes":"This is note"
+    }
+ ,  
+    {  
+        "CreatedByName":"Ankur",
+        "CreatedDate":"05/07/2018",
+        "Notes":"This is note"
+    }
+]
+
+// Advance shipment notes list
+
+
+export const advanceShipmentNotesList = [  
+    {  
+        "ASN":"XYNB",
+        "delivery":"Indore",
+        "shipDate":"05/07/2018",
+        "expectedDelivery":"05/07/2018",
+        "wayBill":"WAYBILL",
+        "tracking":"Indore",
+        "modeOfShipment":"FirstFlight",
+        "shippingMethod":"XYZ",
+        "packingSlip":"ABS",
+        "shipToLocation":"Indore",
+        "totalPrice":"15264"
+    }
+ ,  
+    {  
+        "ASN":"XTZ",
+        "delivery":"Indore",
+        "shipDate":"05/07/2018",
+        "expectedDelivery":"05/07/2018",
+        "wayBill":"WAYBILL",
+        "tracking":"Indore",
+        "modeOfShipment":"FirstFlight",
+        "shippingMethod":"XYZ",
+        "packingSlip":"ABS",
+        "shipToLocation":"Indore",
+        "totalPrice":"15264"
+    }
+]
--- src/app/open-invoices/open-invoices-list/open-invoices-list.component.ts
+++ src/app/open-invoices/open-invoices-list/open-invoices-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { UIHelper } from '../../helpers/ui.helpers';
+import { Commonservice } from '../../services/commonservice.service';
+import { openInvoicesList } from '../../demodata/open-invoices';
+import { GridComponent } from '@progress/kendo-angular-grid';
+import { Configuration } from '../../../assets/configuration';
+import { CurrentSidebarInfo } from '../../models/sidebar/current-sidebar-info';
+import { ModuleName, ComponentName } from '../../enums/enums';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-open-invoices-list',
@@ -7,9 +15,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpenInvoicesListComponent implements OnInit {
 
-  constructor() { }
+  imgPath = Configuration.imagePath;
+
+  pageLimit;
+  pagination:boolean;
+
+  isMobile: boolean;
+  isColumnFilter: boolean = false;
+  isColumnGroup: boolean = false;
+  gridHeight: number;
+  showLoader: boolean = false;
+  searchRequest: string = '';
+
+  getPaginationAttributes(){
+    // pagination add/remove for desktop and mobile
+    let paginationAttributesArray = UIHelper.paginationAttributes();
+    this.pageLimit = paginationAttributesArray[0];
+    this.pagination = paginationAttributesArray[1];
+  }
+  
+  // UI Section
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    // apply grid height
+    this.gridHeight = UIHelper.getMainContentHeight();
+
+    // check mobile device
+    this.isMobile = UIHelper.isMobile();
+
+    this.getPaginationAttributes();
+  }
+  // End UI Section
+
+  
+
+  constructor(private commonService:Commonservice) { }
+
+  public gridData: any[];
 
   ngOnInit() {
+
+    // Apply class on body start
+    const element = document.getElementsByTagName("body")[0];
+    element.className = "";
+    element.classList.add("opti_body-delivery-notes");
+    element.classList.add("opti_body-main-module");
+    // Apply class on body end
+
+    // apply grid height
+    this.gridHeight = UIHelper.getMainContentHeight();
+
+    // check mobile device
+    this.isMobile = UIHelper.isMobile();
+
+    this.getPaginationAttributes();
+
+    
+    this.getOpenInvoicesList();
   }
+
+  /**
+   * Method to get list of inquries from server.
+  */
+  public getOpenInvoicesList() {
+    this.showLoader = true;
+    this.gridData = openInvoicesList;
+    setTimeout(()=>{    
+      this.showLoader = false;
+    }, 1000);
+  }
+
+
+
+  onFilterChange(checkBox:any,grid:GridComponent)
+  {
+    if(checkBox.checked==false){
+      this.clearFilter(grid);
+    }
+  }
+
+  clearFilter(grid:GridComponent){      
+    //grid.filter.filters=[];
+  }
+
+  openInvoiceDetailOnSelection(selection){
+    $('#opti_HomeTabDeliveryNotesID').click(); 
+
+    let currentsideBarInfo: CurrentSidebarInfo = new CurrentSidebarInfo();
+    currentsideBarInfo.ComponentName = ComponentName.DeliveryNotes;
+    currentsideBarInfo.ModuleName = ModuleName.DeliveryNotes;
+    currentsideBarInfo.SideBarStatus = true;
+    this.commonService.setCurrentSideBar(currentsideBarInfo);
+    // Reset Selection.
+    // let selectedSalesOrder = this.gridData[selection.index];
+    // currentsideBarInfo.RequesterData = selectedSalesOrder;
+    // localStorage.setItem("SelectedSalesOrder", JSON.stringify(selectedSalesOrder));
+    // this.commonService.setCurrentSideBar(currentsideBarInfo);
+    // selection.selectedRows=[];  
+  }
+
 
 }
